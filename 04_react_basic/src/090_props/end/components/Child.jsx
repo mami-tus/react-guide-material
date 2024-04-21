63f2502f96712114622cef202e17e9e2a3ceadc5
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-04-21 22:18:11 +0900

    feat: ✨ 35 propsに色々な値を渡す

--- 04_react_basic/src/090_props/end/components/Child.jsx
+++ 04_react_basic/src/090_props/end/components/Child.jsx
@@ -2,13 +2,11 @@
 親コンポーネントで属性値のように記述した各値は、１つのオブジェクトとして子コンポーネントで受け取ることができる。
  */
 
-
-import "./Child.css";
+import './Child.css';
 
 /* POINT Propsを分割代入で受け取る！ */
 /* POINT Propsのデフォルト値を設定する！ */
 const Child = ({ color: c = 'green', num, fn, bool, obj }) => {
-
   // console.log(bool);
 
   return (
