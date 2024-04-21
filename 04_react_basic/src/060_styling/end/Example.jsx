01987267af6a4733dedd12d4fa575750a7f3d94a
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-04-21 16:09:42 +0900

    feat: ✨ 31 JSX内でJSを実行

--- 04_react_basic/src/060_styling/end/Example.jsx
+++ 04_react_basic/src/060_styling/end/Example.jsx
@@ -3,18 +3,20 @@
 */
 
 //  POINT CSS ファイルを import する
-import "./Example.css";
+// ./をで相対パスにすることで、同じディレクトリにあるファイルを指定できる
+// つけなければ、Example.cssというパッケージを指定してることになり node_modules から import することになってしまう
+import './Example.css';
 
 const Example = () => {
   return (
     /* POINT クラス名は class ではなく className で指定する
     HTML のように見えますが、これは JSX と呼ばれる構文です。class 属性は className を使用しましょう。
     */
-  
+
     /* POINT class と className
     React 16 以降では custom DOM attributes をサポートしているため class も使用可能です。（ただ、ウォーニングも出るため、非推奨！！）
     */
-    <div className="component">
+    <div className='component'>
       <h3>Hello Component</h3>
     </div>
   );
