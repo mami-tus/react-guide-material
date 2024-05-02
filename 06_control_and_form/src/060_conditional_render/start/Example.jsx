1803798ebcc0eb29ef96879fd61137ea5b81745f
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-05-03 07:22:11 +0900

    feat: ✨ 65 条件分岐

--- 06_control_and_form/src/060_conditional_render/start/Example.jsx
+++ 06_control_and_form/src/060_conditional_render/start/Example.jsx
@@ -1,27 +1,49 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const Example = () => {
-  const animals = ["Dog", "Cat", "Rat"];
+  const animals = ['Dog', 'Cat', null, 'Rat'];
 
-  const [filterVal, setFilterVal] = useState("");
+  const [filterVal, setFilterVal] = useState('');
 
   return (
     <>
       <input
-        type="text"
+        type='text'
         value={filterVal}
         onChange={(e) => setFilterVal(e.target.value)}
       />
       <ul>
         {animals
           .filter((animal) => {
-            const isMatch = animal.indexOf(filterVal) !== -1;
-            console.log(animal.indexOf(filterVal));
+            const animalStr = animal ?? '';
+            const isMatch = animalStr.indexOf(filterVal) !== -1;
             return isMatch;
           })
-          .map((animal) => (
-            <li key={animal}>{animal}</li>
-          ))}
+          .map((animal) => {
+            // if文
+            // if (animal === 'Dog') {
+            //   return <li key={animal}>{animal}★</li>;
+            // } else {
+            //   return <li key={animal}>{animal}</li>;
+            // }
+            // 三項演算子
+            // return (
+            //   <li key={animal}>{animal + (animal === 'Dog' ? '★' : '')}</li>
+            // );
+            // &&演算子
+            // return (
+            //   <li key={animal}>
+            //     {animal}
+            //     {animal === 'Dog' && '★'}
+            //   </li>
+            // );
+            return (
+              <li key={animal}>
+                {animal ?? 'null,undefinedでした'}
+                {animal === 'Dog' && '★'}
+              </li>
+            );
+          })}
       </ul>
     </>
   );
