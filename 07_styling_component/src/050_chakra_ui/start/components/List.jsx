f5ececa9dca7813e097c2e72e345a2a4facc351b
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-05-05 15:56:33 +0900

    feat: ✨ 83 ChakraUIを使う

--- 07_styling_component/src/050_chakra_ui/start/components/List.jsx
+++ 07_styling_component/src/050_chakra_ui/start/components/List.jsx
@@ -1,19 +1,34 @@
-const List = ({todos, deleteTodo}) => {
-    const complete = (id) => {
-        deleteTodo(id)
-    }
-    return (
-        <div>
-            {todos.map(todo => {
-                return (
-                    <div key={todo.id}>
-                        <button onClick={() => complete(todo.id)}>完了</button>
-                        <span>{todo.content}</span>
-                    </div>
-                )
-            })}
-        </div>
-    );
-}
+import { VStack, StackDivider } from '@chakra-ui/react';
+
+const List = ({ todos, deleteTodo }) => {
+  const complete = (id) => {
+    deleteTodo(id);
+  };
+  return (
+    <VStack
+      divider={<StackDivider />}
+      // color={{
+      //   sm: 'red.600',
+      //   md: 'blue.600',
+      //   lg: 'green.500',
+      //   xl: 'red.600',
+      // }}
+      borderColor='black.100'
+      borderWidth='1px'
+      borderRadius='3px'
+      p={5}
+      alignItems='start'
+    >
+      {todos.map((todo) => {
+        return (
+          <div key={todo.id}>
+            <button onClick={() => complete(todo.id)}>完了</button>
+            <span>{todo.content}</span>
+          </div>
+        );
+      })}
+    </VStack>
+  );
+};
 
 export default List;
--- 07_styling_component/src/050_chakra_ui/start/components/Todo.jsx
+++ 07_styling_component/src/050_chakra_ui/start/components/Todo.jsx
@@ -1,24 +1,24 @@
-import { useState } from "react"
-import List from "./List"
-import Form from "./Form"
+import { useState } from 'react';
+import List from './List';
+import Form from './Form';
 
 const Todo = () => {
   const todosList = [
     {
       id: 1,
-      content: "店予約する",
+      content: '店予約する',
     },
     {
       id: 2,
-      content: "卵買う",
+      content: '卵買う',
     },
     {
       id: 3,
-      content: "郵便出す",
+      content: '郵便出す',
     },
   ];
 
-  const [ todos, setTodos ] = useState(todosList);
+  const [todos, setTodos] = useState(todosList);
 
   const deleteTodo = (id) => {
     const newTodos = todos.filter((todo) => {
@@ -26,17 +26,18 @@ const Todo = () => {
     });
 
     setTodos(newTodos);
-  }
+  };
 
   const createTodo = (todo) => {
     setTodos([...todos, todo]);
-  }
+  };
 
   return (
     <>
-      <List todos={todos} deleteTodo={deleteTodo}/>
-      <Form createTodo={createTodo}/>
+      <h2>Reminder</h2>
+      <List todos={todos} deleteTodo={deleteTodo} />
+      <Form createTodo={createTodo} />
     </>
-  )
+  );
 };
 export default Todo;
