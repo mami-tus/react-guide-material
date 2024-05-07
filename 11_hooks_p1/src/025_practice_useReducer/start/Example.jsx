e5cd34f4dec3b69dcc6d7b6e2c58826bd51ba185
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-05-07 22:42:49 +0900

    feat: ✨ 116 useReducer練習

--- 11_hooks_p1/src/025_practice_useReducer/start/Example.jsx
+++ 11_hooks_p1/src/025_practice_useReducer/start/Example.jsx
@@ -1,8 +1,35 @@
-import { useReducer } from "react";
+import { useReducer } from 'react';
 
-const CALC_OPTIONS = ["add", "minus", "divide", "multiply"];
+const CALC_OPTIONS = ['add', 'minus', 'divide', 'multiply'];
 
-const reducer = () => {}
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'calculate':
+      const { a, b } = state;
+      let result;
+      switch (action.operation) {
+        case 'add':
+          result = a + b;
+          break;
+        case 'minus':
+          result = a - b;
+          break;
+        case 'divide':
+          result = a / b;
+          break;
+        case 'multiply':
+          result = a * b;
+          break;
+        default:
+          result = state.result;
+      }
+      return { ...state, result };
+    case 'set_number':
+      return { ...state, [action.name]: action.value };
+    default:
+      return state;
+  }
+};
 
 const Example = () => {
   const initState = {
@@ -14,22 +41,23 @@ const Example = () => {
   const [state, dispatch] = useReducer(reducer, initState);
 
   const calculate = (e) => {
-    
+    dispatch({ type: 'calculate', operation: e.target.value });
   };
 
   const numChangeHandler = (e) => {
-    
-  }
+    const { name, value } = e.target;
+    dispatch({ type: 'set_number', name, value: Number(value) });
+  };
 
   return (
     <>
-    <h3>練習問題</h3>
-    <p>useReducerを使って完成コードと同じ機能を作成してください。</p>
-      {/* <div>
+      <h3>練習問題</h3>
+      <p>useReducerを使って完成コードと同じ機能を作成してください。</p>
+      <div>
         a:
         <input
-          type="number"
-          name="a"
+          type='number'
+          name='a'
           value={state.a}
           onChange={numChangeHandler}
         />
@@ -37,15 +65,20 @@ const Example = () => {
       <div>
         b:
         <input
-          type="number"
-          name="b"
+          type='number'
+          name='b'
           value={state.b}
           onChange={numChangeHandler}
         />
       </div>
       <select value={state.type} onChange={calculate}>
+        {CALC_OPTIONS.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
       </select>
-      <h1>結果：{state.result}</h1> */}
+      <h3>結果：{state.result}</h3>
     </>
   );
 };
