dbac3a50d6b45d8a45462141e1b736e5392a4b2c
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2024-05-16 22:17:37 +0900

    feat: ✨ 124 修正

--- 11_hooks_p1/src/080_practice_reminder/start/components/Item.jsx
+++ 11_hooks_p1/src/080_practice_reminder/start/components/Item.jsx
@@ -1,12 +1,4 @@
-import { useTodo, useTodoDispatch } from '../context/TodoContext';
-
-const Item = () => {
-  const todo = useTodo();
-  const dispatch = useTodoDispatch();
-
-  const complete = (id) => {
-    dispatch({ type: 'delete', payload: { id } });
-  };
+const Item = ({ todo, complete }) => {
   return (
     <div>
       <button onClick={() => complete(todo.id)}>完了</button>
--- 11_hooks_p1/src/080_practice_reminder/start/components/List.jsx
+++ 11_hooks_p1/src/080_practice_reminder/start/components/List.jsx
@@ -1,10 +1,16 @@
-import { Item } from './Item';
+import { useTodo, useTodoDispatch } from '../context/TodoContext';
+import Item from './Item';
 
 const List = () => {
+  const todo = useTodo();
+  const dispatch = useTodoDispatch();
+  const complete = (id) => {
+    dispatch({ type: 'delete', payload: { id } });
+  };
   return (
     <div>
       {todos.map((todo) => {
-        return <Item key={todo.id} />;
+        return <Item todo={todo} complete={complete} key={todo.id} />;
       })}
     </div>
   );
